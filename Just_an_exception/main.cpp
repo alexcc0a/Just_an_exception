a790b7900a10d18b5489dd80e621247425dcf34a
Alexander Nesterov <alex18nest@icloud.com>
2024-08-13 16:57:17 -0300

    [+] https://github.com/alexcc0a/Just_an_exception.git

--- Just_an_exception/main.cpp
+++ Just_an_exception/main.cpp
@@ -5,10 +5,43 @@
 //  Created by Александр Нестеров on 13/8/24.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class bad_length : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Вы ввели строку запретной длины!";
+    }
+};
+
+int function(const std::string& str, int forbidden_length) {
+    if (str.length() == forbidden_length) {
+        throw bad_length();
+    }
+    return str.length();
+}
+
+int main() {
+    int forbidden_length;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+    std::string word;
+    
+    while (true) {
+        std::cout << "Введите слово: ";
+        std::cin >> word;
+
+        try {
+            int length = function(word, forbidden_length);
+            std::cout << "Длина слова \"" << word << "\" равна " << length << std::endl;
+        } catch (const bad_length& e) {
+            std::cout << e.what() << " До свидания." << std::endl;
+            break;
+        }
+    }
     return 0;
 }
+
